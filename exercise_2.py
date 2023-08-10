8f8f22125d22a0a9f1b1d4496ee25a7eaf2650e0
kick250 <kicklobal@gmail.com>
2023-08-10 16:27:14 -0300

    exercise 2

--- exercise_2.py
+++ exercise_2.py
@@ -1,4 +1,6 @@
-def main(): pass
+def main():
+  for i in range(1, 20 + 1):
+    print(i)
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_2.py
+++ test/test_exercise_2.py
@@ -1,1 +1,6 @@
 import spec
+import exercise_2
+
+def test_print_all_numbers_positive_before_21():
+  numbers = '\n'.join([str(i) for i in range(1, 21)]) + '\n'
+  spec.expect_print(exercise_2.main, numbers)
