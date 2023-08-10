a5fa4586879d87f4daedb83d4e45b23689490285
kick250 <kicklobal@gmail.com>
2023-08-10 16:49:19 -0300

    exercise 4

--- exercise_4.py
+++ exercise_4.py
@@ -1,4 +1,9 @@
-def main(): pass
+LIMITE = 20
+
+
+def main():
+  for i in range(1, (LIMITE + 1), 2):
+    print(i)
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_4.py
+++ test/test_exercise_4.py
@@ -1,1 +1,7 @@
 import spec
+import exercise_4
+
+
+def test_print_all_not_pair_numbers_up_20():
+  result = '\n'.join((str(i) for i in range(1, 21, 2))) + '\n'
+  spec.expect_print(exercise_4.main, result)
