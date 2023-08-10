c3ea67729e88c8b1906afbedee466d93cbea2002
kick250 <kicklobal@gmail.com>
2023-08-10 16:42:40 -0300

    exercise 3

--- exercise_3.py
+++ exercise_3.py
@@ -1,4 +1,15 @@
-def main(): pass
+LIMITE = 1000000
+
+
+def get_numbers():
+  return tuple(range(1, (LIMITE + 1)))
+
+def main():
+  numbers = get_numbers()
+
+  for number in numbers: print(number)
+
+  print(sum(numbers))
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_3.py
+++ test/test_exercise_3.py
@@ -1,1 +1,11 @@
 import spec
+import exercise_3
+
+
+def test_return_all_numbers_up_1000000():
+  assert exercise_3.get_numbers() == tuple(range(1, 1000000 + 1))
+
+def test_print_sum():
+  expected_print = '\n'.join((str(i) for i in exercise_3.get_numbers())) + "\n"
+  expected_print += "500000500000\n"
+  spec.expect_print(exercise_3.main, expected_print)
