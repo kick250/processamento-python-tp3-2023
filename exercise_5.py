2f2f0f09c0373c854ec25a0e5b01182d750649fa
kick250 <kicklobal@gmail.com>
2023-08-11 08:18:41 -0300

    exercise 5

--- exercise_5.py
+++ exercise_5.py
@@ -1,4 +1,13 @@
-def main(): pass
+def main():
+  numbers = []
+  number = 1
+  while len(numbers) < 10:
+    if number % 3 == 0:
+      numbers.append(number)
+    number += 1
+
+  for index, number in enumerate(numbers):
+    print(f"3 x {index + 1} = {number}")
 
 
 if __name__ == "__main__": main()
