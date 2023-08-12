db4dc640b34098641367fd9a594ad32460eade28
kick250 <kicklobal@gmail.com>
2023-08-12 12:58:09 -0300

    exercise 10

--- exercise_10.py
+++ exercise_10.py
@@ -1,4 +1,12 @@
-def main(): pass
+def get_biggest_rivers():
+  return {
+    'Rio Nilo': 'Egito',
+    'Rio Amazonas': 'Brasil',
+    'Rio Yangtzé': 'China',
+  }
+def main():
+  for river, country in get_biggest_rivers().items():
+    print(f"O {river} atravessa o {country}")
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_10.py
+++ test/test_exercise_10.py
@@ -1,1 +1,18 @@
 import spec
+import exercise_10
+
+def test_get_biggest_rivers():
+  assert exercise_10.get_biggest_rivers() == {
+    'Rio Nilo': 'Egito',
+    'Rio Amazonas': 'Brasil',
+    'Rio Yangtzé': 'China',
+  }
+
+def test_main():
+  expected_result = '\n'.join([
+    "O Rio Nilo atravessa o Egito",
+    "O Rio Amazonas atravessa o Brasil",
+    "O Rio Yangtzé atravessa o China\n"
+  ])
+
+  spec.expect_print(exercise_10.main, expected_result)
