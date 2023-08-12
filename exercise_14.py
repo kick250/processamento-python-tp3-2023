5b79d6ba1c2d9a6d8b4abe611aef920fb12a0eff
kick250 <kicklobat@gmail.com>
2023-08-12 17:53:24 -0300

    exercise 14

--- exercise_14.py
+++ exercise_14.py
@@ -1,4 +1,25 @@
-def main(): pass
+NAME_INDEX = 0
+NUMBERS_INDEX = 1
+
+def create_dict(peoples_name_per_numbers):
+  peoples = {}
+
+  for people in peoples_name_per_numbers:
+    peoples[people[NAME_INDEX]] = people[NUMBERS_INDEX]
+
+  return peoples
+
+def main():
+  peoples_name_per_numbers = [
+    ["Ana", [7, 1, 2]],
+    ["Rodrigo", [32, 5, 22]],
+    ["Pablo", [21, 24, 69]]
+  ]
+
+  peoples = create_dict(peoples_name_per_numbers)
+  for key in peoples.keys():
+    print(f"nome: {key}, numero favorito: {peoples[key]};")
+
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_14.py
+++ test/test_exercise_14.py
@@ -1,1 +1,16 @@
 import spec
+import exercise_14
+
+def test_generate_dict():
+  peoples_name_per_numbers = [
+    ["Ana", [7, 1, 2]],
+    ["Rodrigo", [32, 5, 22]],
+    ["Pablo", [21, 24, 69]]
+  ]
+
+  peoples = exercise_14.create_dict(peoples_name_per_numbers)
+  assert peoples == {
+    "Ana": [7, 1, 2],
+    "Rodrigo": [32, 5, 22],
+    "Pablo": [21, 24, 69]
+  }
