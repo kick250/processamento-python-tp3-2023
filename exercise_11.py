0d94ee62a1cd24188d49b537e70f461fa5255f92
kick250 <kicklobal@gmail.com>
2023-08-12 13:41:02 -0300

    exercise 11

--- exercise_11.py
+++ exercise_11.py
@@ -1,4 +1,24 @@
-def main(): pass
+def get_research_result():
+  return {
+    'Jen': 'Python',
+    'Sarah': 'C',
+    'Edward': 'Ruby',
+    'Phil': 'Python'
+  }
+
+def get_message_for(name):
+  research = get_research_result()
+  is_on_research = tuple(research.keys()).count(name) != 0
+  if is_on_research:
+    return f"Obrigado por participar da pesquisa {name}!"
+  else:
+    return f"Participe da nossa pesquisa {name}!"
+
+def main():
+  names = [ 'Jen', 'Ana', 'Sarah', 'Luciana', 'Paulo', 'Edward', 'Phil', 'Yuri' ]
+
+  for name in names:
+    print(get_message_for(name))
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_11.py
+++ test/test_exercise_11.py
@@ -1,1 +1,9 @@
 import spec
+import exercise_11
+
+def test_get_message_for_when_name_in_research():
+  assert exercise_11.get_message_for("Ana") == "Participe da nossa pesquisa Ana!"
+
+def test_get_message_for_when_name_not_in_research():
+  assert exercise_11.get_message_for("Jen") == f"Obrigado por participar da pesquisa Jen!"
+
