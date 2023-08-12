bea0af587c15eee758f490bb814f4ad3cc7b50c3
kick250 <kicklobal@gmail.com>
2023-08-12 14:35:40 -0300

    exercise 13

--- exercise_13.py
+++ exercise_13.py
@@ -1,4 +1,15 @@
-def main(): pass
+def get_peoples():
+  return {
+    "Karen": ["Shopping", "Feira", "Praia"],
+    "Luana": ["Praia", "Pão de açucar", "Praça"],
+    "Joao": ["Pão de açucar", "Casa", "Shopping"]
+  }
+
+
+def main():
+  peoples = get_peoples()
+  for name, lugares in peoples.items():
+    print(f"Os lugares favoritos de {name} são {lugares}")
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_13.py
+++ test/test_exercise_13.py
@@ -1,1 +1,12 @@
 import spec
+import exercise_13
+
+
+def test_create_peoples_dict():
+  peoples = exercise_13.get_peoples()
+
+  assert len(peoples.keys()) == 3
+  assert str(type(peoples)) == "<class 'dict'>"
+
+
+
