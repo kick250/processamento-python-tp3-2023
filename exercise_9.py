f73cb13fa4632e3aeb448945885f1f0eca5582ae
kick250 <kicklobal@gmail.com>
2023-08-12 12:27:48 -0300

    exercise 9

--- exercise_9.py
+++ exercise_9.py
@@ -1,4 +1,16 @@
-def main(): pass
+def main():
+  terms = {
+    "Variavel": "Nomes que apontam para um local da memória que guarda um valor que definimos.",
+    "Array": "Conjunto de elementos.",
+    "Dicionario": "Conjunto de elementos organizados por chave valor.",
+    "função": "Termo que guarda um conjunto de instruções de código.",
+    "Linguagem de programação": "Metodo padronizado usado para passar instruções para o computador"
+  }
+
+  for key, value in terms.items():
+    print(key)
+    print(value)
+    print()
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_9.py
+++ test/test_exercise_9.py
@@ -1,1 +1,19 @@
 import spec
+import exercise_9
+
+
+def test_main():
+  expected_print = '\n'.join([
+    "Variavel",
+    "Nomes que apontam para um local da memória que guarda um valor que definimos.\n",
+    "Array",
+    "Conjunto de elementos.\n",
+    "Dicionario",
+    "Conjunto de elementos organizados por chave valor.\n",
+    "função",
+    "Termo que guarda um conjunto de instruções de código.\n",
+    "Linguagem de programação",
+    "Metodo padronizado usado para passar instruções para o computador\n\n",
+  ])
+
+  spec.expect_print(exercise_9.main, expected_print)
