5e3fa58ad49808b36e3f3cc72a1e579ed0288c92
kick250 <kicklobal@gmail.com>
2023-08-12 13:54:58 -0300

    exercise 12

--- exercise_12.py
+++ exercise_12.py
@@ -1,4 +1,27 @@
-def main(): pass
+from exercise_7 import create_people
+
+
+def get_peoples():
+  return [
+    create_people("Ana", "Silva", 32, "Rio de Janeiro"),
+    create_people("Breno", "Lobato", 23, "São Paulo"),
+    create_people("Yuri", "Silveira", 12, "Niteroi")
+  ]
+
+def get_people_message(people):
+  message = ""
+  message += f"name: {people['first_name']}, "
+  message += f"sobrenome: {people['last_name']}, "
+  message += f"idade: {people['age']}, "
+  message += f"cidade: {people['city']}"
+
+  return message
+
+def main():
+  peoples = get_peoples()
+  for people in peoples:
+    print(get_people_message(people))
+
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_12.py
+++ test/test_exercise_12.py
@@ -1,1 +1,6 @@
 import spec
+import exercise_12
+
+def test_message():
+  people = exercise_12.create_people("Ana", "Silva", 32, "Rio de Janeiro")
+  assert exercise_12.get_people_message(people) == "name: Ana, sobrenome: Silva, idade: 32, cidade: Rio de Janeiro"
