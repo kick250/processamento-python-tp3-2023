b507f252698d987598efd14c651ca9736162bc28
kick250 <kicklobal@gmail.com>
2023-08-12 11:40:35 -0300

    exercise 7

--- test/test_exercise_7.py
+++ test/test_exercise_7.py
@@ -1,1 +1,15 @@
 import spec
+import exercise_7
+
+
+def test_create_people():
+  people = exercise_7.create_people(
+    "Ana",
+    "Silva",
+    32,
+    "Rio de Janeiro"
+  )
+  assert people["first_name"] == "Ana"
+  assert people["last_name"] == "Silva"
+  assert people["age"] == 32
+  assert people["city"] == "Rio de Janeiro"
