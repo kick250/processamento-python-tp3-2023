12bc8535957f5b7a837960346a80688297d8b851
kick250 <kicklobat@gmail.com>
2023-08-12 18:08:17 -0300

    exercise 15

--- exercise_15.py
+++ exercise_15.py
@@ -1,4 +1,24 @@
-def main(): pass
+def create_city(country, population, fact):
+  return {
+    'country': country,
+    'population': population,
+    'fact': fact
+  }
+
+def get_cities():
+  return {
+    "Rio de Janeiro": create_city('Brasil', 6747000, "Tem muitas Praias"),
+    "Salvador": create_city('Brasil', 2886698, "Fica na Bahia"),
+    "Niteroi": create_city('Brasil', 511786, "Tem muitas Praias"),
+  }
+
+def main():
+  cities = get_cities()
+  for city_name, data in cities.items():
+    print(city_name)
+    print(data['country'])
+    print(data['population'])
+    print(data['fact'])
 
 
 if __name__ == "__main__": main()
--- test/test_exercise_15.py
+++ test/test_exercise_15.py
@@ -1,1 +1,10 @@
 import spec
+import exercise_15
+
+
+def test_create_city():
+  assert exercise_15.create_city('Brasil', 6747000, "Tem muitas Praias") == {
+    'country': 'Brasil',
+    'population': 6747000,
+    'fact': "Tem muitas Praias"
+  }
